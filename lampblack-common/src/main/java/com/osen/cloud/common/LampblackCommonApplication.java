6ac9c7955a8b066111cfafb2fff1198d8589ef86
tinypy <1306359812@qq.com>
2019-08-27 16:18:31 +0800

    构建项目

---
+++ lampblack-common/src/main/java/com/osen/cloud/common/LampblackCommonApplication.java
@@ -0,0 +1,13 @@
+package com.osen.cloud.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LampblackCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LampblackCommonApplication.class, args);
+    }
+
+}
