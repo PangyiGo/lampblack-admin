d4310c804f8bd355e87c5f9a5dd031358a467683
tinypy <1306359812@qq.com>
2019-08-28 09:10:07 +0800

    增加lampblack system核心模块

---
+++ lampblack-system/src/main/java/com/osen/cloud/system/LampblackSystemApplication.java
@@ -0,0 +1,13 @@
+package com.osen.cloud.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LampblackSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LampblackSystemApplication.class, args);
+    }
+
+}
