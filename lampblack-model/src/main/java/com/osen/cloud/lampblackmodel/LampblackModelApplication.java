7b51d8183abbf3603b615390e58d63099228aca0
tinypy <1306359812@qq.com>
2019-08-28 09:58:12 +0800

    新提交

---
+++ lampblack-model/src/main/java/com/osen/cloud/lampblackmodel/LampblackModelApplication.java
@@ -0,0 +1,13 @@
+package com.osen.cloud.lampblackmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LampblackModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LampblackModelApplication.class, args);
+    }
+
+}
