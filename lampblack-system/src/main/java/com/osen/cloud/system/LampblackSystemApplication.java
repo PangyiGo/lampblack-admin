7d60350d6e1d76499a1a7eb0092aec796a4ce6c9
tinypy <1306359812@qq.com>
2019-08-28 11:22:50 +0800

    新提交

--- lampblack-system/src/main/java/com/osen/cloud/system/LampblackSystemApplication.java
+++ lampblack-system/src/main/java/com/osen/cloud/system/LampblackSystemApplication.java
@@ -3,10 +3,13 @@ package com.osen.cloud.system;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 // 扫描Mapper配置注解
 @MapperScan(basePackages = {"com.osen.cloud.model"})
+// 开启事务处理
+@EnableTransactionManagement
 public class LampblackSystemApplication {
 
     public static void main(String[] args) {
