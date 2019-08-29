1dd6e32f4fc13cf023eee48191d079ac2d256dd1
tinypy <1306359812@qq.com>
2019-08-29 14:53:28 +0800

    授权认证实现

--- lampblack-service/src/main/java/com/osen/cloud/service/authorization/impl/AuthorizationServiceImpl.java
+++ lampblack-service/src/main/java/com/osen/cloud/service/authorization/impl/AuthorizationServiceImpl.java
@@ -1,11 +1,12 @@
 package com.osen.cloud.service.authorization.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.osen.cloud.common.entity.User;
 import com.osen.cloud.model.authorization.AuthorizationMapper;
 import com.osen.cloud.service.authorization.AuthorizationService;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * User: PangYi
@@ -14,12 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
  * Description: 授权认证服务实现类
  */
 @Service
-@Transactional
 public class AuthorizationServiceImpl extends ServiceImpl<AuthorizationMapper, User> implements AuthorizationService {
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        User user = null;
+        try {
+            // 查询多条记录，抛出异常
+            LambdaQueryWrapper<User> wrapper = Wrappers.<User>lambdaQuery().select(User::getAccount).eq(User::getAccount, username);
+            user = super.getOne(wrapper, true);
+        } catch (Exception e) {
+            return null;
+        }
+        return user;
     }
 
 }
--- lampblack-system/src/test/java/com/osen/cloud/system/LampblackSystemApplicationTests.java
+++ lampblack-system/src/test/java/com/osen/cloud/system/LampblackSystemApplicationTests.java
@@ -1,7 +1,10 @@
 package com.osen.cloud.system;
 
+import com.osen.cloud.common.entity.User;
+import com.osen.cloud.service.authorization.AuthorizationService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class LampblackSystemApplicationTests {
 
+    @Autowired
+    private AuthorizationService authorizationService;
+
     @Test
     public void contextLoads() {
+        User test01 = authorizationService.findByUsername("test01");
+
+        System.out.println(test01);
     }
 
 }
