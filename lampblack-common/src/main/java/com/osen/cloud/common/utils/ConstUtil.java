988cd780cd80fdc0b5886f5b878aa3347a1203b2
tinypy <1306359812@qq.com>
2019-08-30 11:41:29 +0800

    授权认证实现

---
+++ lampblack-common/src/main/java/com/osen/cloud/common/utils/ConstUtil.java
@@ -0,0 +1,13 @@
+package com.osen.cloud.common.utils;
+
+/**
+ * User: PangYi
+ * Date: 2019-08-30
+ * Time: 10:08
+ * Description: 全局静态变量
+ */
+public class ConstUtil {
+
+    // 用户初始密码
+    public static final String INIT_PASSWORD = "$2a$10$yNCkJq5IlAVX07aK2eD2kOoxZhxz7ym.FKjaTK7MimhMRASiUvfkS";
+}
