4d157bbd78b351fc1487c9da44d3820f6fb2e31b
tinypy <1306359812@qq.com>
2019-09-03 14:29:21 +0800

    授权认证实现

---
+++ lampblack-system/src/main/java/com/osen/cloud/system/security/utils/TransferUserToJwt.java
@@ -0,0 +1,31 @@
+package com.osen.cloud.system.security.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: PangYi
+ * Date: 2019-09-03
+ * Time: 13:58
+ * Description: jwt信息结构体
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class TransferUserToJwt implements Serializable {
+
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private List<String> authority = new ArrayList<>(0);
+}
