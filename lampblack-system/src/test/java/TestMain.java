7c225f54d5e6eb66c52c64eba5f7a7ee5853f2ca
tinypy <1306359812@qq.com>
2019-10-22 15:22:20 +0800

    netty服务端代码重构

---
+++ lampblack-system/src/test/java/TestMain.java
@@ -0,0 +1,14 @@
+import com.osen.cloud.common.enums.LampblackSensorCode;
+
+/**
+ * User: PangYi
+ * Date: 2019-10-22
+ * Time: 15:20
+ * Description:
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println(LampblackSensorCode.PM.getCode());
+    }
+}
