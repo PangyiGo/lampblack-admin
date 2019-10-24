b5a735d7eb21ecb4a5bf10315607024899a04d2e
tinypy <1306359812@qq.com>
2019-10-24 16:42:50 +0800

    系统多环境配置

--- lampblack-common/src/main/java/com/osen/cloud/common/entity/system_device/Device.java
+++ lampblack-common/src/main/java/com/osen/cloud/common/entity/system_device/Device.java
@@ -24,7 +24,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@TableName("system_device")
+@TableName("device")
 public class Device extends Model<Device> implements Serializable {
 
     @TableId(type = IdType.AUTO)
