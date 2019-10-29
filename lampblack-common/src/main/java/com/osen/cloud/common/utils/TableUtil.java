9981b176102909739e13ab6841e15776833e736a
tinypy <1306359812@qq.com>
2019-10-29 10:11:25 +0800

    提交

---
+++ lampblack-common/src/main/java/com/osen/cloud/common/utils/TableUtil.java
@@ -0,0 +1,32 @@
+package com.osen.cloud.common.utils;
+
+/**
+ * User: PangYi
+ * Date: 2019-10-29
+ * Time: 9:53
+ * Description: 数据表工具类
+ */
+public class TableUtil {
+
+    /*
+     * VOC设备数据表名称
+     */
+    public static String VocHsitory = "voc_history";
+
+    public static String VocMinute = "voc_minute";
+
+    public static String VocHour = "voc_hour";
+
+    public static String VocDay = "voc_day";
+
+    /*
+     * 冷链设备数据表名称
+     */
+    public static String ColdHistory = "coldchain_history";
+
+    public static String ColdMinute = "coldchain_minute";
+
+    public static String ColdHour = "coldchain_hour";
+
+    public static String ColdDay = "coldchain_day";
+}
