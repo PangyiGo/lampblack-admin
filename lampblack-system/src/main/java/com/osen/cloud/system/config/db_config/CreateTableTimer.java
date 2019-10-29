a633f252a0a5fa1da759f86e99958445c5eb2848
tinypy <1306359812@qq.com>
2019-10-29 10:55:22 +0800

    提交

--- lampblack-system/src/main/java/com/osen/cloud/system/config/db_config/CreateTableTimer.java
+++ lampblack-system/src/main/java/com/osen/cloud/system/config/db_config/CreateTableTimer.java
@@ -1,10 +1,15 @@
 package com.osen.cloud.system.config.db_config;
 
 import com.osen.cloud.common.utils.ConstUtil;
+import com.osen.cloud.common.utils.TableUtil;
 import com.osen.cloud.service.data.lampblack.DataDayService;
 import com.osen.cloud.service.data.lampblack.DataHistoryService;
 import com.osen.cloud.service.data.lampblack.DataHourService;
 import com.osen.cloud.service.data.lampblack.DataMinuteService;
+import com.osen.cloud.service.data.vocs.VocDayService;
+import com.osen.cloud.service.data.vocs.VocHistoryService;
+import com.osen.cloud.service.data.vocs.VocHourService;
+import com.osen.cloud.service.data.vocs.VocMinuteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -38,12 +43,24 @@ public class CreateTableTimer {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    @Autowired
+    private VocHistoryService vocHistoryService;
+
+    @Autowired
+    private VocMinuteService vocMinuteService;
+
+    @Autowired
+    private VocHourService vocHourService;
+
+    @Autowired
+    private VocDayService vocDayService;
+
     /**
      * 每月1号的零点10秒执行
      */
     @Scheduled(cron = "10 0 0 1 * ? ")
     public void taskCreateTable() {
-        log.info("当前时间：" + LocalDateTime.now() + ": 定时创建当前下一月份数据表");
+        log.info("当前时间：" + LocalDateTime.now() + ": 定时创建油烟设备当前下一月份数据表");
 
         dataHistoryService.createNewTable(ConstUtil.createNextTableName(ConstUtil.REALTIME_TB));
 
@@ -52,6 +69,22 @@ public class CreateTableTimer {
         dataHourService.createNewTable(ConstUtil.createNextTableName(ConstUtil.HOUR_TB));
 
         dataDayService.createNewTable(ConstUtil.createNextTableName(ConstUtil.DAY_TB));
+    }
+
+    /**
+     * 每月1号的零点10秒执行
+     */
+    @Scheduled(cron = "15 0 0 1 * ? ")
+    public void vocTask() {
+        log.info("当前时间：" + LocalDateTime.now() + ": 定时创建VOC设备当前下一月份数据表");
+
+        vocHistoryService.createNewTable(ConstUtil.createNextTableName(TableUtil.VocHistory));
+
+        vocMinuteService.createNewTable(ConstUtil.createNextTableName(TableUtil.VocMinute));
+
+        vocHourService.createNewTable(ConstUtil.createNextTableName(TableUtil.VocHour));
+
+        vocDayService.createNewTable(ConstUtil.createNextTableName(TableUtil.VocDay));
     }
 
     /**
