6c995dbd08e357c53e1b9783ec9bb3f9a47faf9f
tinypy <1306359812@qq.com>
2019-10-29 10:08:23 +0800

    提交

--- lampblack-system/src/test/java/com/osen/cloud/system/dev_data/DataHistoryTest.java
+++ lampblack-system/src/test/java/com/osen/cloud/system/dev_data/DataHistoryTest.java
@@ -1,10 +1,15 @@
 package com.osen.cloud.system.dev_data;
 
 import com.osen.cloud.common.utils.ConstUtil;
+import com.osen.cloud.common.utils.TableUtil;
 import com.osen.cloud.service.data.lampblack.DataDayService;
 import com.osen.cloud.service.data.lampblack.DataHistoryService;
 import com.osen.cloud.service.data.lampblack.DataHourService;
 import com.osen.cloud.service.data.lampblack.DataMinuteService;
+import com.osen.cloud.service.data.vocs.VocDayService;
+import com.osen.cloud.service.data.vocs.VocHistoryService;
+import com.osen.cloud.service.data.vocs.VocHourService;
+import com.osen.cloud.service.data.vocs.VocMinuteService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +38,31 @@ public class DataHistoryTest {
     @Autowired
     private DataDayService dataDayService;
 
+    @Autowired
+    private VocHistoryService vocHistoryService;
+
+    @Autowired
+    private VocMinuteService vocMinuteService;
+
+    @Autowired
+    private VocHourService vocHourService;
+
+    @Autowired
+    private VocDayService vocDayService;
+
     @Test
     public void test01() {
         dataHistoryService.createNewTable(ConstUtil.currentTableName(ConstUtil.REALTIME_TB));
         dataMinuteService.createNewTable(ConstUtil.currentTableName(ConstUtil.MINUTE_TB));
         dataHourService.createNewTable(ConstUtil.currentTableName(ConstUtil.HOUR_TB));
         dataDayService.createNewTable(ConstUtil.currentTableName(ConstUtil.DAY_TB));
+    }
+
+    @Test
+    public void test02() {
+        vocHistoryService.createNewTable(ConstUtil.currentTableName(TableUtil.VocHsitory));
+        vocMinuteService.createNewTable(ConstUtil.currentTableName(TableUtil.VocMinute));
+        vocHourService.createNewTable(ConstUtil.currentTableName(TableUtil.VocHour));
+        vocDayService.createNewTable(ConstUtil.currentTableName(TableUtil.VocDay));
     }
 }
