f25643612520756f2f3c25a906dba0ef47644103
tinypy <1306359812@qq.com>
2020-01-14 09:35:28 +0800

    设备实时数据查询

--- lampblack-system/src/test/java/com/osen/cloud/system/LampblackSystemApplicationTests.java
+++ lampblack-system/src/test/java/com/osen/cloud/system/LampblackSystemApplicationTests.java
@@ -2,30 +2,17 @@ package com.osen.cloud.system;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LampblackSystemApplicationTests {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Test
     public void contextLoads() {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        list.stream().filter(x -> x % 2 == 0).map(y -> y * 2).forEach(System.out::println);
-    }
 
-    @Test
-    public void test01() {
-        System.out.println(passwordEncoder.encode("123456"));
     }
 
 }
