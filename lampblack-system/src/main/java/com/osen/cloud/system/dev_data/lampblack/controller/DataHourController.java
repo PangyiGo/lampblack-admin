19e6c3002c47ef7a7f4864174977df32f7145c69
tinypy <1306359812@qq.com>
2020-01-15 15:47:04 +0800

    设备实时数据查询

--- lampblack-system/src/main/java/com/osen/cloud/system/dev_data/lampblack/controller/DataHourController.java
+++ lampblack-system/src/main/java/com/osen/cloud/system/dev_data/lampblack/controller/DataHourController.java
@@ -126,7 +126,7 @@ public class DataHourController {
         // 构建数据表
         List<String> queryTableName = ConstUtil.queryTableName(startDate, endDate, ConstUtil.HOUR_TB);
         for (String tableName : queryTableName) {
-            if (ConstUtil.compareToTime(tableName, MonthCode.Lampblack.getMonth()))
+            if (ConstUtil.compareToTime( MonthCode.Lampblack.getMonth()))
                 continue;
             MybatisPlusConfig.TableName.set(tableName);
             List<DataHour> history = dataHourService.queryDataHourByDate(startDate, endDate, deviceNo, type);
